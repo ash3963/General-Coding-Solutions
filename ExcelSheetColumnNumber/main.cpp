2aad7f3990cd67483e6bc5f56186dd87d9889a75
ash3963 <104307630+ash3963@users.noreply.github.com>
2022-07-29 03:47:00 +0200

    Add files via upload

---
+++ ExcelSheetColumnNumber/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int titleToNumber(string columnTitle){
+        
+        int result=0;
+        for(char c: columnTitle){
+            int d=c-'A'+1;
+            result= result*26+d;
+        }
+        return result;
+}
