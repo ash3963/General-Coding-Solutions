9437759c0877c826eb41f3499740d311355d6cd6
ash3963 <104307630+ash3963@users.noreply.github.com>
2022-08-02 14:00:39 +0200

    Add files via upload

---
+++ MaximumDepthOfBinaryTree/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode* root) {
+    if(!root)
+        return NULL;
+    int l = maxDepth(root->left);
+    int r = maxDepth(root->right);
+    return max(l,r)+1;        
+}
