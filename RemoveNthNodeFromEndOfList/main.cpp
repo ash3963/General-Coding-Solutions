69524f3cd461e45c0e4d88abaa34d1cc15967ef6
ash3963 <104307630+ash3963@users.noreply.github.com>
2022-08-09 11:11:03 +0200

    Add files via upload

---
+++ RemoveNthNodeFromEndOfList/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    if(!head) return NULL;
+    ListNode* start = new ListNode();
+    start->next = head;
+    ListNode* fast = start;
+    ListNode* slow = start;
+    for(int i=1; i<=n; i++) {
+        fast = fast->next;
+    }
+    while(fast->next!=NULL) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    slow->next=slow->next->next;
+    return start->next;
+}
