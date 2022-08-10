0150fe8112cb45aaa32a89e7947f75a439b491eb
ash3963 <104307630+ash3963@users.noreply.github.com>
2022-08-10 12:21:43 +0200

    Add files via upload

---
+++ RotateImage/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix) {
+    reverse(matrix.begin(),matrix.end());
+    for(int i = 0; i < matrix.size(); i++) {
+        for(int j = i + 1; j < matrix[i].size(); j++)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
