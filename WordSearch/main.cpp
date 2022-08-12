dc4d37218ef3b672be385f656972126f4bbda1e6
ash3963 <104307630+ash3963@users.noreply.github.com>
2022-08-12 11:19:48 +0200

    Add files via upload

---
+++ WordSearch/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool dfs(vector<vector<char>>& board, string word, int i, int j, int n) {
+    if(n == word.size()) return true;
+    if(i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[n]) return false;
+    board[i][j] = '0';
+    bool status = dfs(board, word, i+1, j, n+1) || dfs(board, word, i-1, j, n+1) || dfs(board, word, i, j+1, n+1) || dfs(board, word, i, j-1, n+1);
+    board[i][j] = word[n];
+    return status;
+}
+
+
+bool exist(vector<vector<char>>& board, string word) {
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board[i].size(); j++) {
+            if(board[i][j] == word[0] && dfs(board, word, i, j, 0))
+                return true;
+        }
+    }
+    return false;
+}
+
