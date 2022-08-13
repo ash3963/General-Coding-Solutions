b68670858b461dcca49b4e088543c3a54b8c2ad7
ash3963 <104307630+ash3963@users.noreply.github.com>
2022-08-13 12:09:01 +0200

    Add files via upload

---
+++ LowestCommonAncestorOfABinarySearchTree/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if((root->val < p->val) && (root->val < q->val))
+        return lowestCommonAncestor(root->right, p, q);
+    if((root->val > p->val) && (root->val > q->val))
+        return lowestCommonAncestor(root->left, p, q);
+    return root;
+}
