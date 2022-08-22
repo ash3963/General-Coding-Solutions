b1f7e2486e54e99c646fa13fa39c016582ea4360
ash3963 <104307630+ash3963@users.noreply.github.com>
2022-08-22 20:06:32 +0200

    Add files via upload

---
+++ SearchA2DMatrixII/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size(), n = m ? matrix[0].size() : 0, r = 0, c = n - 1;
+    while(r < m && c >= 0) {
+        if(matrix[r][c] == target) return true;
+        matrix[r][c] > target ? c-- : r++;
+    }
+    return false;
+}
